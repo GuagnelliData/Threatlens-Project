95bb7613c47057211061b34b51df3b5375929852
Carlos Joseph Guagnelli <joseph.guagnelli@gmail.com>
2025-07-08 13:08:38 -0600

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,21 +1,30 @@
-# src/model_nn.py
-
+import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 
-def train_neural_network(X_train, y_train, X_test, y_test):
-    y_train_cat = to_categorical(y_train)
-    y_test_cat = to_categorical(y_test)
+def one_hot_encode(y_train, y_test, num_classes):
+    y_train_cat = to_categorical(y_train, num_classes)
+    y_test_cat = to_categorical(y_test, num_classes)
+    return y_train_cat, y_test_cat
 
+def build_model(input_shape, num_classes):
     model = Sequential([
-        Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
+        Dense(128, activation='relu', input_shape=(input_shape,)),
+        Dropout(0.3),
         Dense(64, activation='relu'),
-        Dense(y_train_cat.shape[1], activation='softmax')
+        Dropout(0.3),
+        Dense(num_classes, activation='softmax')
     ])
-
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-    model.fit(X_train, y_train_cat, epochs=10, batch_size=64, validation_data=(X_test, y_test_cat))
-
     return model
+
+def train_model(model, X_train, y_train_cat, X_test, y_test_cat, epochs, batch_size):
+    history = model.fit(
+        X_train, y_train_cat,
+        validation_data=(X_test, y_test_cat),
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=1
+    )
+    return history
