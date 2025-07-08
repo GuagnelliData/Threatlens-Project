f2844a5d2e2cdf1ab1ce241a614e93f9bd2eedf2
Carlos Joseph Guagnelli <joseph.guagnelli@gmail.com>
2025-07-08 13:07:28 -0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,10 @@
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data" / "raw"
+CSV_FILE = DATA_DIR / "MachineLearningCVE" / "Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"
+
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
+BATCH_SIZE = 256
+EPOCHS = 10
